a5cd0f856847df5dcb35f686e4d8470e01ec6891
happypsyduck <53825716+happypsyduck@users.noreply.github.com>
2019-10-19 18:36:59 -0400

    Made full proxy

--- web/index.php
+++ web/index.php
@@ -1,10 +1,25 @@
 <?php
 date_default_timezone_set("UTC");
-$destination_url = "http://happypsyduck.mywebcommunity.org/interface/";
+$destination_url = "http://happypsyduck.mywebcommunity.org/interface/"; // The absolute URL this file is going to
+
 if(isset($_GET["request"])){
+		
+	$query = $_SERVER['QUERY_STRING'];
+	// Get cURL resource
+	$curl = curl_init();
+	// Set some options - we are passing in a useragent too here
+	curl_setopt_array($curl, [
+		CURLOPT_RETURNTRANSFER => 1,
+		CURLOPT_URL => $destination_url.'?'.$query,
+		CURLOPT_USERAGENT => 'FiatDex User'
+	]);
+	// Send the request & save response to $resp
+	$resp = curl_exec($curl);
+	// Close request to clear up some resources
+	curl_close($curl);
+	
 	header("Content-Type: application/javascript"); //Force browser to see it as Javascript
-  $query = $_SERVER['QUERY_STRING'];
-  echo $query;
+	echo $resp;
 }else{
   echo "This FiatDex database must be queried by JSONP";
 }
